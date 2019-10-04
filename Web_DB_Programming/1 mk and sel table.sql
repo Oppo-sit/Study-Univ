63f2a31ee8101e7c6ebc2e85c4938a5c07854596
Oppo-sit <44254655+Oppo-sit@users.noreply.github.com>
2019-10-04 13:25:47 +0900

    Add files via upload

---
+++ Web_DB_Programming/1 mk and sel table.sql
@@ -0,0 +1,5 @@
+create table `tbl`(Name char(5));
+
+select * from tbl;
+select name from tbl;
+select * from tbl where name='seung min';
---
+++ Web_DB_Programming/3 view.sql
@@ -0,0 +1,3 @@
+create view uv_memberTBL as
+	select memberName, memberAddress
+	from membertbl;
