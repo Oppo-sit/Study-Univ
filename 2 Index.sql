dac42c5ab329311d507e1ad99239d47e84426868
Oppo-sit <44254655+Oppo-sit@users.noreply.github.com>
2019-10-04 13:24:13 +0900

    Delete 2 Index.sql

--- 2 Index.sql
+++
@@ -1,8 +0,0 @@
-create TABLE indextblindexTBL(first_name varchar(14), last_name varchar(16), hire_data date); 
-
-CREATE INDEX idx_indexTBL_firstname
-	ON indexTBL(first_name);
-
-SELECT * FROM indextbl;
-SELECT * FROM indextbl WHERE first_name = 'mary';
-
